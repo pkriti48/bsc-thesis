9fd7ba39ef2449a974c3f444ade57462217e6ae9
Prakriti Sabharwal <sabharwalprakriti@gmail.com>
2025-12-25 12:46:30 +0100

    Add AppendAndConcat.lean

--- Game/Levels/AppendAndConcat.lean
+++ Game/Levels/AppendAndConcat.lean
@@ -8,15 +8,17 @@ World "AppendAndConcat"
 Title "The Append and Concat World"
 
 Introduction "
-In the first world of the game, you will start by proving that appending the empty word ```nil```
-to any ```word``` results in ```word```.
+In the first world of the game, you will prove some facts about the functions: ```append```,
+```concat```, ```replicateChar``` and ```replicateWord```.
+
+You will first prove that appending the empty word ```nil``` to any ```word``` results in
+```word```.
 
 Then, you will prove how the length of a word is calculated when two words are connected using
 ```append``` or a character ```char``` is connected to a word using ```concat```.
 
-Finally, building upon these lemmas, you will prove how the length of a word looks like when this
-word corresponds to ```n``` replicas of a character ```char``` or ```n``` replicas of a word
-```word```.
+Finally, building upon these lemmas, you will show how the length of a word is influenced when
+it consists of ```n``` replicas of a character ```char``` or ```n``` replicas of some ```word```.
 
 Let's get started!
 "
